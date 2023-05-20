7cb565c2d9316b92a0a0f1d8bfaba4ddfe431405
Syed abdullah mateen <fix404error@gmail.com>
2023-05-20 17:55:08 +0500

    prime website

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,98 @@
+<!-- ======= Footer ======= -->
+
+<footer class="section bg-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="">
+                    <h6 class="footer-heading text-uppercase text-white">Information</h6>
+                    <ul class="list-unstyled footer-link mt-4">
+                        <li><a href="">Pages</a></li>
+                        <li><a href="">Our Team</a></li>
+                        <li><a href="">Feuchers</a></li>
+                        <li><a href="">Pricing</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="">
+                    <h6 class="footer-heading text-uppercase text-white">Ressources</h6>
+                    <ul class="list-unstyled footer-link mt-4">
+                        <li><a href="">Monitoring Grader </a></li>
+                        <li><a href="">Video Tutorial</a></li>
+                        <li><a href="">Term &amp; Service</a></li>
+                        <li><a href="">Zeeko API</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-lg-2">
+                <div class="">
+                    <h6 class="footer-heading text-uppercase text-white">Help</h6>
+                    <ul class="list-unstyled footer-link mt-4">
+                        <li><a href="">Sign Up </a></li>
+                        <li><a href="">Login</a></li>
+                        <li><a href="">Terms of Services</a></li>
+                        <li><a href="">Privacy Policy</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="">
+                    <h6 class="footer-heading text-uppercase text-white">Contact Us</h6>
+                    <p class="contact-info mt-4">Contact us if need help withanything</p>
+                    <p class="contact-info">+01 123-456-7890</p>
+                    <div class="mt-5">
+                        <ul class="list-inline">
+
+                            <li class="list-inline-item"><a href="#"><i class="bi bi-facebook"></i></a>
+                            </li>
+                            <li class="list-inline-item"><a href="#"><i class="bi bi-twitter"></i></a></li>
+                            <li class="list-inline-item"><a href="#"><i class="bi bi-instagram"></i></a>
+                            </li>
+                            <li class="list-inline-item"><a href="#"><i class="bi bi-linkedin"></i></a>
+                            </li>
+
+
+                        </ul>
+
+
+
+
+
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="text-center mt-5">
+        <p class="footer-alt mb-0 f-14">2023 © <a href="https://swipencart.com/">swipencart.com</a> , All Rights
+            Reserved</p>
+    </div>
+</footer>
+<!-- ======= Footer End======= -->
+<a href="https://api.whatsapp.com/send?phone=03056041005"
+    class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-whatsapp"
+        style="font-size:68px;color:rgb(9, 255, 0)"></i></a>
+<!-- <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a> -->
+
+<!-- Vendor JS Files -->
+<script src="{{ url('assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+<script src="{{ url('assets/vendor/aos/aos.js') }}"></script>
+<script src="{{ url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ url('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+<script src="{{ url('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+<script src="{{ url('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+<script src="{{ url('assets/vendor/php-email-form/validate.js') }}"></script>
+
+<!-- Template Main JS File -->
+<script src="{{ url('assets/js/main.js') }}"></script>
+
+</body>
+
+</html>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Prime liberty</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="{{asset('assets/img/p-logo.png')}}" rel="icon">
+    <!-- <link href="{{url('assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon"> -->
+
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="{{url('assets/vendor/aos/aos.css')}}" rel="stylesheet">
+    <link href="{{url('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{url('assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+    <link href="{{url('assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+    <link href="{{url('assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+    <link href="{{url('assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
+    <link href="{{url('assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="{{url('assets/css/style.css')}}" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top d-flex align-items-center">
+        <div class="container d-flex align-items-center justify-content-between">
+
+            <div class="logo">
+                <h1><a href="index.html">Vesperr</a></h1>
+                <!-- Uncomment below if you prefer to use an image logo -->
+
+            </div>
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link scrollto active" href="#">Home</a></li>
+                    <li><a class="nav-link scrollto" href="#about">About</a></li>
+
+
+                    <li><a class="nav-link scrollto" href="#products">Products</a></li>
+
+                    <li><a class="nav-link scrollto" href="#team">Team</a></li>
+
+                    <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+                    <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav>
+
+            <!-- .navbar -->
+
+        </div>
+    </header><!-- End Header -->
